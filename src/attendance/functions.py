65c7de06bc34336675f99fef958061c261d050ee
Saiful Islam <saiful.kbtel@gmail.com>
2022-08-30 15:28:17 +0600

    Feat: add functions.check_holidays()

--- src/attendance/functions.py
+++ src/attendance/functions.py
@@ -1,4 +1,5 @@
 import re
+from attendance.db import Holidays
 from flask import session
 
 #Convert all team names of Fiber & Support to generic name
@@ -15,3 +16,23 @@ def convert_team_name():
         team_name = session['team']
     
     return team_name
+
+#Check holiday in holidays table
+def check_holidays(name, start_date, end_date=None):
+    holiday_name_exists = Holidays.query.filter_by(name=name).all()
+    if holiday_name_exists:
+        return 'Holiday name exists'
+        
+    holiday_start_date_exists = Holidays.query.filter(Holidays.start_date<=start_date, Holidays.end_date>=start_date).first()
+    if holiday_start_date_exists:
+        return 'Holiday start date overlaps with another holiday'
+    
+    if end_date:
+        if start_date != end_date:
+            holiday_end_date_exists = Holidays.query.filter(Holidays.start_date<=end_date, Holidays.end_date>=end_date).first()
+            if holiday_end_date_exists:
+                return 'Holiday end date overlaps with another holiday'
+
+            any_date_exists = Holidays.query.filter(Holidays.start_date>start_date, Holidays.end_date<end_date).first()
+            if any_date_exists:
+                return 'Holiday start and/or end dates overlaps with other holidays'
