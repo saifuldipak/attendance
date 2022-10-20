e0e4313f945564c668cb5e5eb8b5254cb5279def
Saiful Islam <saiful.kbtel@gmail.com>
2022-10-20 12:22:15 +0600

    Feat: add module application.py

---
+++ src/attendance/application.py
@@ -0,0 +1,48 @@
+from flask import Blueprint, flash, render_template, url_for, session, redirect, current_app
+from .auth import login_required, team_leader_required
+from .db import db, Applications, Employee, AttendanceSummary
+from .functions import approval_authorization, check_attendance_summary, check_available_leave, get_emails
+from .mail import send_mail2
+
+application = Blueprint('application', __name__)
+
+@application.route('/application/approve/<application_id>')
+@login_required
+@team_leader_required
+def approve(application_id):
+    if session['role'] == 'Head':
+        application_for = 'department'
+    elif session['role'] in ('Supervisor', 'Manager'):
+        application_for = 'team'
+
+    application = Applications.query.filter_by(id=application_id).one()
+    
+    can_approve = approval_authorization(application)
+    if not can_approve:
+        current_app.logger.error(' approval(): "%s" trying to approve application "%s"', session['username'], application_id)
+        flash('You are not authorized to approve this application', category='error')
+        return render_template(url_for('leave.search_application', application_for=application_for))
+
+    attendance_summary = check_attendance_summary(application.start_date, application.end_date)
+    if attendance_summary:
+        msg = f'Attendance summary already prepared for {application.start_date.strftime("%B")},{application.start_date.year}' 
+        flash(msg, category='error')
+        return render_template(url_for('leave.search_application', application_for=application_for))
+
+    if application.type == 'Casual' or application.type == 'Medical':
+        available = check_available_leave(application, 'update')
+        if not available:
+            flash('Leave not available, please check leave summary', category='error')
+            return render_template(url_for('leave.search_application', application_for='team'))
+    
+    application.status = 'Approved'
+    db.session.commit()
+    flash('Leave approved', category='message')
+    
+    emails = get_emails(application)
+    rv = send_mail2(sender=emails['sender'], receiver=emails['receiver'], cc=emails['cc'], application=application, type='leave', action='approved')
+    if rv:
+        current_app.logger.warning(rv)
+        flash('Failed to send mail', category='warning')
+
+    return redirect(url_for('leave.search_application', application_for=application_for))
