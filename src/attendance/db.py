eaa685969569c09ff8905246d933298a3ae88c86
Saiful Islam <saiful.kbtel@gmail.com>
2022-10-27 15:11:42 +0600

    Fix: remove relationship from Employee to ApprLeaveAttn in db module

--- src/attendance/db.py
+++ src/attendance/db.py
@@ -18,7 +18,7 @@ class Employee(db.Model):
     applications = db.relationship('Applications', cascade='delete, merge, save-update', backref='employee', lazy=True)
     leaveavailable = db.relationship('LeaveAvailable', cascade='delete, merge, save-update', backref='employee', lazy=True)
     attendance = db.relationship('Attendance', cascade='delete, merge, save-update', backref='employee', lazy=True)
-    approved = db.relationship('ApprLeaveAttn', cascade='delete, merge, save-update', backref='employee', lazy=True)
+    #approved = db.relationship('ApprLeaveAttn', cascade='delete, merge, save-update', backref='employee', lazy=True)
     attendancesummary = db.relationship('AttendanceSummary', cascade='delete, merge, save-update', backref='employee', lazy=True)
     dutyschedule = db.relationship('DutySchedule', cascade='delete, merge, save-update', backref='employee', lazy=True)
 
