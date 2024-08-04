a35f2ab428dd0c1a7cbf2e862a15fcf2e2ef793c
Saiful Islam <saiful.kbtel@gmail.com>
2024-08-04 19:41:32 +0600

    add class EmployFiscalYear

--- src/attendance/schemas.py
+++ src/attendance/schemas.py
@@ -5,3 +5,8 @@ from typing import Optional
 class AnnualLeave(BaseModel):
     joining_date: date
     new_fiscal_year_start_date: Optional[date] = None
+
+class EmployeeFiscalYear(BaseModel):
+    employee: object
+    fiscal_year_start_date: date
+    fiscal_year_end_date: date
