634ea4c0e661297498982ba8ad6e18d020303316
Saiful Islam <saiful.kbtel@gmail.com>
2024-08-14 12:02:58 +0600

    add validate_config() function to validate app config

--- src/attendance/__init__.py
+++ src/attendance/__init__.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from flask import Flask
 from datetime import timedelta
 import logging
@@ -6,10 +7,31 @@ from logging.config import dictConfig
 from flask.logging import default_handler
 import yaml
 from flask_migrate import Migrate
+from typing import Dict, Any, Type
+
+def validate_config(app):
+    CONFIG_SCHEMA: Dict[str, Type[Any]] = {
+        'SECRET_KEY': str,
+        'PERMANENT_SESSION_LIFETIME': int,
+        'SMTP_HOST': str,
+        'SMTP_PORT':str,
+        'CASUAL': int,
+        'MEDICAL': int,
+        'EARNED': int,
+        'IN_TIME': str,
+        'OUT_TIME': str,
+        'IN_GRACE_TIME': int,
+        'OUT_GRACE_TIME': int
+    }
+    for key, expected_type in CONFIG_SCHEMA.items():
+        if key not in app.config:
+            raise ValueError(f"Missing required configuration key: {key}")
+        
+        if not isinstance(app.config[key], expected_type):
+            raise TypeError(f"Configuration key {key} has wrong type: expected {expected_type}, got {type(app.config[key])}")
+
 
 def create_app(test_config=None):
-    
-    #initializing application
     app = Flask(__name__, instance_relative_config=True)
     
     #updating config values
@@ -36,9 +58,6 @@ def create_app(test_config=None):
         except OSError as e:
             app.logger.error(e)
 
-    #loading configuration from file
-    app.config.from_pyfile('config.py', silent=True)
-    
     #loading logging config
     logging_cfg = os.path.join(app.instance_path, 'logging.yaml')
     if os.path.exists(logging_cfg):
@@ -46,6 +65,14 @@ def create_app(test_config=None):
         for logger in (app.logger, logging.getLogger('sqlalchemy'), logging.getLogger('wtforms')):
             logger.addHandler(default_handler)
     
+    #loading configuration from file
+    app.config.from_pyfile('config.py', silent=True)
+    try:
+        validate_config(app)
+    except (ValueError, TypeError) as e:
+        app.logger.error(f"Configuration error: {e}")
+        sys.exit(1) 
+        
     #registering sqlalchemy
     from .db import db
     db.init_app(app)
